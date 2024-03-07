1dd7a1cb30450b2020f6a9141e3b5f13773dfcc7
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-07 17:56:58 +0100

    install tailwind UI library and create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
 })
-export class AppComponent {
-  title = 'mediumclone_angular';
-}
+export class AppComponent {}
