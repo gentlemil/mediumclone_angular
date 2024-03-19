3014f49ae57da2c8124015f4a412d4a5e8ab3e52
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-19 16:33:15 +0100

    create store for favorites

--- src/app/shared/components/addToFavorites/addToFavorites.component.ts
+++ src/app/shared/components/addToFavorites/addToFavorites.component.ts
@@ -1,24 +1,34 @@
-import {CommonModule} from '@angular/common'
-import {Component, Input} from '@angular/core'
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addToFavoritesActions } from './store/actions';
 
 @Component({
+  standalone: true,
   selector: 'mc-add-to-favorites',
   templateUrl: './addToFavorites.component.html',
-  standalone: true,
   imports: [CommonModule],
 })
 export class AddToFavoritesComponent {
-  @Input() isFavorited: boolean = false
-  @Input() favoritesCount: number = 0
-  @Input() articleSlug: string = ''
+  @Input() isFavorited: boolean = false;
+  @Input() favoritesCount: number = 0;
+  @Input() articleSlug: string = '';
+
+  constructor(private store: Store) {}
 
   handleLike(): void {
+    this.store.dispatch(
+      addToFavoritesActions.addToFavorites({
+        isFavorited: this.isFavorited,
+        slug: this.articleSlug,
+      })
+    );
     if (this.isFavorited) {
-      this.favoritesCount = this.favoritesCount - 1
+      this.favoritesCount = this.favoritesCount - 1;
     } else {
-      this.favoritesCount = this.favoritesCount + 1
+      this.favoritesCount = this.favoritesCount + 1;
     }
 
-    this.isFavorited = !this.isFavorited
+    this.isFavorited = !this.isFavorited;
   }
 }
