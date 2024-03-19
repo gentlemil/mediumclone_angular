779c8a04a51e629183d3051ea715913b84b7b542
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-19 16:20:49 +0100

    prepare add to favorite component

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -17,6 +17,7 @@ import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
 import queryString from 'query-string';
 import { TagListComponent } from '../tagList/tagList.component';
 import { environment } from '../../../../environments/environment';
+import { AddToFavoritesComponent } from '../addToFavorites/addToFavorites.component';
 
 @Component({
   selector: 'mc-feed',
@@ -29,6 +30,7 @@ import { environment } from '../../../../environments/environment';
     LoadingComponent,
     PaginationComponent,
     TagListComponent,
+    AddToFavoritesComponent,
   ],
 })
 export class FeedComponent implements OnInit, OnChanges {
